d1b5e3b5d418ed42ba2057ee165fdd047aa3b760
chrisBalher <c.baldeon.h@gmail.com>
2021-04-12 09:42:24 -0500

    Assignment 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
 import {DISHES} from './shared/dishes';
@@ -8,7 +7,7 @@ import {DISHES} from './shared/dishes';
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = {
+    this.state={
       dishes: DISHES
     };
   }
@@ -17,13 +16,13 @@ class App extends Component {
       <div className="App">
         <Navbar dark color='primary'>
           <div className='container'>
-            <NavbarBrand href='/'>Ristorante</NavbarBrand>
+            <NavbarBrand href='#'>Ristorante</NavbarBrand>
           </div>
         </Navbar>
-        <Menu dishes = {this.state.dishes} />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
-  } 
+  }
 }
 
 export default App;
---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import { Card, CardImg,  CardText, CardBody, CardTitle } from 'reactstrap';
+
+class DishDetail extends Component {
+    constructor(props){
+        super(props);
+    }
+    renderDish(dish){
+        if (dish != null) {
+            return(
+                <Card className=" col-12 col-md-5 m-1">
+                    <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }else{
+            return(
+                <div></div>
+            );
+        }
+    }
+    renderComments(dish){
+        if (dish != null) {
+            const commentsAll = dish.comments.map((comment)=>{
+                let dt= new Date(comment.date)
+                return (
+                    <ul className="list-unstyled">
+                        <li>{comment.comment}</li>
+                        <li>-- {comment.author}, { dt.toLocaleString('en-us', { month: 'short' })+' '+dt.getDay()+','+dt.getFullYear()}</li>
+                    </ul>
+                );
+            });
+            return(
+                <div className=" col-12 col-md-5 m-1 text-left ml-3">
+                    <h4>Comments</h4>
+                    {commentsAll}
+                </div>
+            );
+        }else{
+            return(
+                <div></div>
+            );
+        }
+    }
+    render(){
+        return (
+            <div className="row">
+                    {this.renderDish(this.props.dish )}
+                    {this.renderComments(this.props.dish)}
+            </div>
+        );
+    }
+}
+
+export default DishDetail;
