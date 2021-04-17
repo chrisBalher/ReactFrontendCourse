28712cd2cc043c74426105b001179ca15acb9eaf
chrisBalher <c.baldeon.h@gmail.com>
2021-04-16 19:10:31 -0500

    React Redux Form

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Card, CardImg,  CardText, CardTitle, Breadcrumb, BreadcrumbItem, CardBody, CardHeader, Media, CardSubtitle } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { LEADERS } from '../shared/leaders';
 
 function RenderLeader( {leader} ) {
     return (
